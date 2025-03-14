9c1e4d22196d06e082edd7ae1e6369e7e9836312
okppop <okppop@protonmail.com>
2025-03-14 16:35:38 +0800

    feat: RLocker for multiple redis nodes
    - feat: add RLocker struct for multiple redis nodes
    - test: add test for RLocker
    - test: fix errChan listening in sleep_to_TTL sub test

---
+++ rlocker.go
@@ -0,0 +1,396 @@
+package lockerd
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var (
+	// ErrNotMoreThanHalfNodes returns when unable acquire more
+	// than half of redis nodes, only returned from TryLock*.
+	ErrNotMoreThanHalfNodes = errors.New("unable acquire more than half of redis nodes, acquire lock fail therefor")
+)
+
+// RLocker represent distributed lock for multiple redis nodes,
+// implement redlock.
+type RLocker struct {
+	// clients provides a list of redis nodes.
+	clients []*redis.Client
+	// opts control lock's behavior.
+	opts Options
+	// internal field
+	_currentValue string
+	_minAcquired  int
+	_maxAcquired  int
+}
+
+// NewRLocker create a RLocker with cliens slice and opts, fields
+// in opts weren't specified will be replaced with default value,
+// besides Key which must specify, see Options and
+// Options.complete.
+//
+// clients at least have three client.
+func NewRLocker(clients []*redis.Client, opts Options) (*RLocker, error) {
+	n := len(clients)
+	if n < 3 {
+		return nil, errors.New("the number of clients is smaller than 3")
+	}
+
+	c := make([]*redis.Client, n)
+	copy(c, clients)
+
+	opts, err := opts.complete()
+	if err != nil {
+		return nil, err
+	}
+
+	return &RLocker{
+		clients:      c,
+		opts:         opts,
+		_minAcquired: n/2 + 1,
+		_maxAcquired: n,
+	}, nil
+}
+
+// Lock keep trying to acquire lock until ctx is canceled or
+// deadline exceeded, ensure passing a proper ctx to avoid
+// infinite retries.
+//
+// To unlock lock, call UnlockFunc that returned.
+//
+// Retry interval between two attempts according to
+// RLocker.opts.RetryInterval.
+//
+// Only possible error returned is context.Canceled and
+// context.DeadlineExceeded.
+func (l *RLocker) Lock(ctx context.Context) (UnlockFunc, error) {
+	value := l.opts.ValueGeneratorFunc()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+			unlock, err := l.tryLock(ctx, value)
+			if err == ErrNotMoreThanHalfNodes {
+				unlock(ctx)
+				<-time.After(l.opts.RetryInterval)
+				continue
+			}
+
+			return unlock, nil
+		}
+	}
+}
+
+// TryLock try to acquire lock once, return a UnlockFunc and
+// ErrNotMoreThanHalfNodes when can't acquire lock at least half
+// of redis nodes.
+//
+// UnlockFunc should be call even ErrNotMoreThanHalfNodes was
+// returned, to unlock the redis nodes that acquire success in
+// process.
+func (l *RLocker) TryLock(ctx context.Context) (UnlockFunc, error) {
+	return l.tryLock(ctx, l.opts.ValueGeneratorFunc())
+}
+
+// LockWithAutoRenewal keep trying to acquire lock until ctx is
+// canceled or deadline exceeded, ensure passing a proper ctx
+// to avoid infinite retries.
+//
+// To unlock lock, call UnlockFunc that returned.
+//
+// Retry interval between two attempts according to
+// RLocker.opts.RetryInterval.
+//
+// Only possible error returned is context.Canceled and
+// context.DeadlineExceeded.
+//
+// If success, multiple goroutinues will be started to auto
+// renewal the lock for avoiding lock expire when reach
+// RLocker.opts.TTL, the read-only channel returned will pass
+// error which happend in auto renewal goroutinues, the error
+// returned from channel was wrapped or joined with redis node's
+// info, therefor use errors.Is to estimate what happend rather
+// than err == Errxxx.
+//
+// UnlockFunc should be call even ErrNotMoreThanHalfNodes was
+// returned, to unlock the redis nodes that acquire success in
+// process.
+func (l *RLocker) LockWithAutoRenewal(ctx context.Context) (UnlockFunc, <-chan error, error) {
+	value := l.opts.ValueGeneratorFunc()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, nil, ctx.Err()
+		default:
+			unlock, errChan, err := l.tryLockWithAutoRenewal(ctx, value)
+			if err == ErrNotMoreThanHalfNodes {
+				unlock(ctx)
+				<-time.After(l.opts.RetryInterval)
+				continue
+			}
+
+			return unlock, errChan, nil
+		}
+	}
+}
+
+// TryLockWithAutoRenewal try to acquire lock once, return a
+// UnlockFunc and ErrNotMoreThanHalfNodes when can't acquire lock
+// at least half of redis nodes.
+//
+// If success, multiple goroutinues will be started to auto
+// renewal the lock for avoiding lock expire when reach
+// RLocker.opts.TTL, the read-only channel returned will pass
+// error which happend in auto renewal goroutinues, the error
+// returned from channel was wrapped or joined with redis node's
+// info, therefor use errors.Is to estimate what happend rather
+// than err == Errxxx.
+//
+// UnlockFunc should be call even ErrNotMoreThanHalfNodes was
+// returned, to unlock the redis nodes that acquire success in
+// process.
+func (l *RLocker) TryLockWithAutoRenewal(ctx context.Context) (UnlockFunc, <-chan error, error) {
+	return l.tryLockWithAutoRenewal(ctx, l.opts.ValueGeneratorFunc())
+}
+
+func (l *RLocker) tryLock(ctx context.Context, value string) (UnlockFunc, error) {
+	successClients := make([]*redis.Client, 0, l._maxAcquired)
+	var successClientMutex sync.Mutex
+
+	var wg sync.WaitGroup
+	wg.Add(l._maxAcquired)
+
+	for _, client := range l.clients {
+		go func(client *redis.Client) {
+			defer wg.Done()
+
+			ok, err := client.SetNX(ctx, l.opts.Key, value, l.opts.TTL).Result()
+			// connection error, or ctx done.
+			if err != nil {
+				return
+			}
+
+			// key exists, SET fail.
+			if !ok {
+				return
+			}
+
+			successClientMutex.Lock()
+			defer successClientMutex.Unlock()
+
+			successClients = append(successClients, client)
+		}(client)
+	}
+
+	wg.Wait()
+
+	if len(successClients) >= l._minAcquired {
+		l._currentValue = value
+		return l.pUnlockFunc(successClients, value), nil
+	} else {
+		return l.pUnlockFuncWhenFail(successClients, value), ErrNotMoreThanHalfNodes
+	}
+}
+
+func (l *RLocker) tryLockWithAutoRenewal(ctx context.Context, value string) (UnlockFunc, <-chan error, error) {
+	successClients := make([]*redis.Client, 0, l._maxAcquired)
+	var successClientMutex sync.Mutex
+
+	var wg sync.WaitGroup
+	wg.Add(l._maxAcquired)
+
+	for _, client := range l.clients {
+		go func(client *redis.Client) {
+			defer wg.Done()
+
+			ok, err := client.SetNX(ctx, l.opts.Key, value, l.opts.TTL).Result()
+			// connection err, or ctx done.
+			if err != nil {
+				return
+			}
+
+			// key exists, SET fail.
+			if !ok {
+				return
+			}
+
+			successClientMutex.Lock()
+			defer successClientMutex.Unlock()
+
+			successClients = append(successClients, client)
+		}(client)
+	}
+
+	wg.Wait()
+
+	if len(successClients) >= l._minAcquired {
+		l._currentValue = value
+
+		// for control auto renewal goroutinue's life cycle.
+		autoRenewalCtx, cancelAutoRenewal := context.WithCancel(context.Background())
+		errChan := make(chan error, len(successClients))
+
+		go l.autoRenewal(autoRenewalCtx, successClients, value, errChan)
+
+		return l.pUnlockFuncWithAutoRenewal(successClients, value, cancelAutoRenewal), errChan, nil
+	} else {
+		return l.pUnlockFuncWhenFail(successClients, value), nil, ErrNotMoreThanHalfNodes
+	}
+}
+
+func (l *RLocker) pUnlockFunc(clients []*redis.Client, value string) UnlockFunc {
+	return func(ctx context.Context) error {
+		l._currentValue = ""
+
+		return l.unlock(ctx, clients, value)
+	}
+}
+
+func (l *RLocker) pUnlockFuncWithAutoRenewal(clients []*redis.Client, value string, cancelAutoRenewal context.CancelFunc) UnlockFunc {
+	return func(ctx context.Context) error {
+		l._currentValue = ""
+		cancelAutoRenewal()
+
+		return l.unlock(ctx, clients, value)
+	}
+}
+
+func (l *RLocker) pUnlockFuncWhenFail(clients []*redis.Client, value string) UnlockFunc {
+	return func(ctx context.Context) error {
+		return l.unlock(ctx, clients, value)
+	}
+}
+
+func (l *RLocker) unlock(ctx context.Context, clients []*redis.Client, value string) error {
+	var finalErr error
+	var finalErrMutex sync.Mutex
+
+	var wg sync.WaitGroup
+	wg.Add(len(clients))
+
+	for _, client := range clients {
+		go func(client *redis.Client) {
+			defer wg.Done()
+
+			res, err := client.Eval(ctx, unlockScript, []string{l.opts.Key}, value).Result()
+			// GET key returns nil.
+			if err == redis.Nil {
+				err = fmt.Errorf("%s: %w", client.String(), ErrLockKeyIsNotSet)
+
+				finalErrMutex.Lock()
+				defer finalErrMutex.Unlock()
+
+				finalErr = errors.Join(finalErr, err)
+				return
+			}
+
+			// connection error, or ctx done.
+			if err != nil {
+				err = fmt.Errorf("%s: %w", client.String(), err)
+
+				finalErrMutex.Lock()
+				defer finalErrMutex.Unlock()
+
+				finalErr = errors.Join(finalErr, err)
+				return
+			}
+
+			switch res.(type) {
+			// redis returns the number of keys it successfully
+			// delete.
+			//
+			// if return 1, everything is fine.
+			//
+			// if return 0, barely happend, maybe key expired
+			// during unlock, I believe it's fine, since the key
+			// must doesn't exist now.
+			case int64:
+				return
+			// redis doesn't return a number means lock is
+			// acquired by others.
+			default:
+				err = fmt.Errorf("%s: %w", client.String(), ErrLockIsAcquired)
+
+				finalErrMutex.Lock()
+				defer finalErrMutex.Unlock()
+
+				finalErr = errors.Join(finalErr, err)
+				return
+			}
+		}(client)
+	}
+
+	wg.Wait()
+
+	return finalErr
+}
+
+func (l *RLocker) autoRenewal(ctx context.Context, clients []*redis.Client, value string, errChan chan<- error) {
+	defer close(errChan)
+
+	var wg sync.WaitGroup
+	wg.Add(len(clients))
+
+	for _, client := range clients {
+		go func(client *redis.Client) {
+			defer wg.Done()
+
+			for {
+				select {
+				// unlock was called
+				case <-ctx.Done():
+					return
+				case <-time.After(l.opts.AutoRenewalInterval):
+					res, err := client.Eval(ctx, renewalScript, []string{l.opts.Key}, value, int64(l.opts.AutoRenewalTTL/time.Second)).Result()
+
+					switch err {
+					// unlock was called during renewal.
+					case context.Canceled, context.DeadlineExceeded:
+						return
+					// Key is not set, maybe expired.
+					case redis.Nil:
+						errChan <- fmt.Errorf("%s: %w", client.String(), ErrLockKeyIsNotSet)
+						return
+					case nil:
+						switch res.(type) {
+						// renewal success.
+						case int64:
+							continue
+						// lock is acquired by others, take this
+						// error seriously.
+						default:
+							errChan <- fmt.Errorf("%s: %w", client.String(), ErrLockIsAcquired)
+							return
+						}
+					// probably network issue, check error for
+					// details.
+					default:
+						errChan <- fmt.Errorf("%s: %w", client.String(), err)
+						return
+					}
+				}
+			}
+		}(client)
+	}
+
+	wg.Wait()
+}
+
+// GetValue should only be called when you are acquired the
+// lock, otherwise you may get other owner's value instead
+// of empty string.
+func (l *RLocker) GetValue() string {
+	return l._currentValue
+}
+
+func (l RLocker) GetOptionsCopy() Options {
+	return l.opts
+}
--- rlocker_test.go
+++ rlocker_test.go
@@ -2,6 +2,7 @@ package lockerd
 
 import (
 	"context"
+	"errors"
 	"sync"
 	"testing"
 	"time"
@@ -9,19 +10,16 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-var client *redis.Client = redis.NewClient(&redis.Options{
-	Network:  "tcp",
-	Addr:     "127.0.0.1:6379",
-	Password: "",
-	DB:       0,
-})
+var clients []*redis.Client = []*redis.Client{
+	redis.NewClient(&redis.Options{Addr: "127.0.0.1:6379", DB: 0}),
+	redis.NewClient(&redis.Options{Addr: "127.0.0.1:6379", DB: 1}),
+	redis.NewClient(&redis.Options{Addr: "127.0.0.1:6379", DB: 2}),
+}
 
-func TestLock(t *testing.T) {
-	l, err := NewLocker(client, Options{
-		Key: "k1",
-	})
+func TestRLocker_Lock(t *testing.T) {
+	l, err := NewRLocker(clients, Options{Key: "k1"})
 	if err != nil {
-		t.Error("NewLocker error:", err)
+		t.Error("NewRLocker error:", err)
 	}
 	ctx := context.Background()
 
@@ -46,8 +44,8 @@ func TestLock(t *testing.T) {
 		<-time.After(6 * time.Second)
 
 		err = unlock(ctx)
-		if err != ErrLockKeyIsNotSet {
-			t.Error("lock should be expired")
+		if !errors.Is(err, ErrLockKeyIsNotSet) {
+			t.Error("error should contains ErrLockKeyIsNotSet")
 		}
 	})
 
@@ -56,12 +54,12 @@ func TestLock(t *testing.T) {
 		defer cancel()
 
 		unlock, err := l.Lock(ctx)
-		if err != context.DeadlineExceeded {
+		if !errors.Is(err, context.DeadlineExceeded) {
 			t.Error("error should be context.DeadlineExceeded")
 		}
 
 		if unlock != nil {
-			t.Error("unlock should nil")
+			t.Error("unlock should be nil")
 		}
 	})
 
@@ -70,20 +68,21 @@ func TestLock(t *testing.T) {
 
 		for range 1000 {
 			wg.Add(1)
+
 			go func() {
 				defer wg.Done()
 
 				unlock, err := l.Lock(ctx)
 				if err != nil {
-					t.Error("Lock error")
+					t.Error("lock error")
 				} else {
 					if l.GetValue() == "" {
-						t.Error("value shouldn't be empty before unlock")
+						t.Error("value shouldn't be empty")
 					}
 
-					err := unlock(ctx)
+					err = unlock(ctx)
 					if err != nil {
-						t.Error("Unlock error")
+						t.Error("unlock error")
 					}
 				}
 			}()
@@ -93,19 +92,17 @@ func TestLock(t *testing.T) {
 	})
 }
 
-func TestTryLock(t *testing.T) {
-	l, err := NewLocker(client, Options{
-		Key: "k1",
-	})
+func TestRLocker_TryLock(t *testing.T) {
+	l, err := NewRLocker(clients, Options{Key: "k1"})
 	if err != nil {
-		t.Error("NewLocker error:", err)
+		t.Error("NewRLocker error:", err)
 	}
 	ctx := context.Background()
 
 	t.Run("lock and unlock", func(t *testing.T) {
 		unlock, err := l.TryLock(ctx)
 		if err != nil {
-			t.Error("lock err")
+			t.Error("lock error")
 		}
 
 		err = unlock(ctx)
@@ -123,8 +120,8 @@ func TestTryLock(t *testing.T) {
 		<-time.After(6 * time.Second)
 
 		err = unlock(ctx)
-		if err != ErrLockKeyIsNotSet {
-			t.Error("lock should be expired")
+		if !errors.Is(err, ErrLockKeyIsNotSet) {
+			t.Error("lock should be expire")
 		}
 	})
 
@@ -133,12 +130,21 @@ func TestTryLock(t *testing.T) {
 		defer cancel()
 
 		unlock, err := l.TryLock(ctx)
-		if err != context.DeadlineExceeded {
-			t.Error("error should be context.DeadlineExceeded")
+		if err != ErrNotMoreThanHalfNodes {
+			t.Error("error shoule be ErrNotMoreThanHalfNodes")
+		}
+
+		if l.GetValue() != "" {
+			t.Error("_currentValue should be empty")
 		}
 
 		if unlock != nil {
-			t.Error("unlock should nil")
+			err = unlock(context.Background())
+			if err != nil {
+				t.Error("unlock error")
+			}
+		} else {
+			t.Error("unlock is nil")
 		}
 	})
 
@@ -150,22 +156,25 @@ func TestTryLock(t *testing.T) {
 		defer unlock(ctx)
 
 		unlock, err = l.TryLock(ctx)
-		if err != ErrLockIsAcquired {
-			t.Error("error should be ErrLockIsAcquired")
+		if err != ErrNotMoreThanHalfNodes {
+			t.Error("error should be ErrNotMoreThanHalfNodes")
 		}
 
 		if unlock != nil {
-			t.Error("unlock should be nil")
+			err = unlock(ctx)
+			if err != nil {
+				t.Error("unlock error")
+			}
+		} else {
+			t.Error("unlock is nil")
 		}
 	})
 }
 
-func TestLockWithAutoRenewal(t *testing.T) {
-	l, err := NewLocker(client, Options{
-		Key: "k1",
-	})
+func TestRLocker_LockWithAutoRenewal(t *testing.T) {
+	l, err := NewRLocker(clients, Options{Key: "k1"})
 	if err != nil {
-		t.Error("NewLocker error:", err)
+		t.Error("NewRLocker error")
 	}
 	ctx := context.Background()
 
@@ -177,21 +186,16 @@ func TestLockWithAutoRenewal(t *testing.T) {
 		if errChan == nil {
 			t.Error("errChan shouldn't be nil")
 		}
-		if len(errChan) != 0 {
-			t.Error("errChan len is not 0")
-		}
-		if cap(errChan) != 1 {
-			t.Error("errChan cap is not 1")
-		}
+
+		go func() {
+			for err := range errChan {
+				t.Error(err)
+			}
+		}()
 
 		err = unlock(ctx)
 		if err != nil {
 			t.Error("unlock error")
-		}
-
-		_, ok := <-errChan
-		if ok {
-			t.Error("errChan should be closed and no value inside")
 		}
 	})
 
@@ -203,11 +207,11 @@ func TestLockWithAutoRenewal(t *testing.T) {
 		if err != context.DeadlineExceeded {
 			t.Error("error should be context.DeadlineExceeded")
 		}
-		if unlock != nil {
-			t.Error("unlock should be nil")
-		}
 		if errChan != nil {
 			t.Error("errChan should be nil")
+		}
+		if unlock != nil {
+			t.Error("unlock should be nil")
 		}
 	})
 
@@ -220,27 +224,18 @@ func TestLockWithAutoRenewal(t *testing.T) {
 
 		go func(errChan <-chan error) {
 			for range errChan {
-				t.Error("errChan send a error")
+				t.Error(err)
 			}
 		}(errChan)
-
-		if unlock == nil {
-			t.Error("unlock is nil")
-		}
 
 		<-time.After(6 * time.Second)
 
 		unlock, errChan, err = l.TryLockWithAutoRenewal(ctx)
-		if err != ErrLockIsAcquired {
-			t.Error("error should be ErrLockIsAcquired")
+		if err != ErrNotMoreThanHalfNodes {
+			t.Error("lock error should be ErrNotMoreThanHalfNodes")
 		}
-
 		if errChan != nil {
 			t.Error("errChan should be nil")
-		}
-
-		if unlock != nil {
-			t.Error("unlock should be nil")
 		}
 	})
 
@@ -249,7 +244,10 @@ func TestLockWithAutoRenewal(t *testing.T) {
 
 		for range 500 {
 			wg.Add(1)
+
 			go func() {
+				defer wg.Done()
+
 				unlock, errChan, err := l.LockWithAutoRenewal(ctx)
 				if err != nil {
 					t.Error("lock error")
@@ -267,31 +265,6 @@ func TestLockWithAutoRenewal(t *testing.T) {
 						t.Error("errChan send a error")
 					}
 				}
-
-				wg.Done()
-			}()
-		}
-
-		wg.Wait()
-	})
-
-	t.Run("concurrency 2", func(t *testing.T) {
-		wg := sync.WaitGroup{}
-
-		for range 200 {
-			wg.Add(1)
-			go func() {
-				unlock, _, err := l.LockWithAutoRenewal(ctx)
-				if err != nil {
-					t.Error("lock error")
-				} else {
-					err := unlock(ctx)
-					if err != nil {
-						t.Error("unlock error")
-					}
-				}
-
-				wg.Done()
 			}()
 		}
 
@@ -299,35 +272,29 @@ func TestLockWithAutoRenewal(t *testing.T) {
 	})
 }
 
-func TestTryLockWithAutoRenewal(t *testing.T) {
-	l, err := NewLocker(client, Options{
-		Key: "k1",
-	})
+func TestRLocker_TryLockWithAutoRenewal(t *testing.T) {
+	l, err := NewRLocker(clients, Options{Key: "k1"})
 	if err != nil {
-		t.Error("NewLocker error:", err)
+		t.Error("NewRLocker error")
 	}
 	ctx := context.Background()
 
 	t.Run("lock and unlock", func(t *testing.T) {
 		unlock, errChan, err := l.TryLockWithAutoRenewal(ctx)
 		if err != nil {
-			t.Error("lock err")
+			t.Error("lock error")
 		}
 
-		wg := sync.WaitGroup{}
-		wg.Add(1)
 		go func() {
 			for range errChan {
 				t.Error("errChan send a error")
 			}
-			wg.Done()
 		}()
 
 		err = unlock(ctx)
 		if err != nil {
 			t.Error("unlock error")
 		}
-		wg.Wait()
 	})
 
 	t.Run("sleep to TTL", func(t *testing.T) {
@@ -335,24 +302,28 @@ func TestTryLockWithAutoRenewal(t *testing.T) {
 		if err != nil {
 			t.Error("lock error")
 		}
+		defer unlock(ctx)
 
-		wg := sync.WaitGroup{}
-		wg.Add(1)
-		go func() {
+		go func(errChan <-chan error) {
 			for range errChan {
 				t.Error("errChan send a error")
 			}
-			wg.Done()
-		}()
+		}(errChan)
 
 		<-time.After(6 * time.Second)
+
+		unlock, errChan, err = l.TryLockWithAutoRenewal(ctx)
+		if err != ErrNotMoreThanHalfNodes {
+			t.Error("error should be ErrNotMoreThanHalfNodes")
+		}
+		if errChan != nil {
+			t.Error("errChan should be nil")
+		}
 
 		err = unlock(ctx)
 		if err != nil {
 			t.Error("unlock error")
 		}
-
-		wg.Wait()
 	})
 
 	t.Run("context DeadlineExceeded", func(t *testing.T) {
@@ -360,16 +331,15 @@ func TestTryLockWithAutoRenewal(t *testing.T) {
 		defer cancel()
 
 		unlock, errChan, err := l.TryLockWithAutoRenewal(ctx)
-		if err != context.DeadlineExceeded {
-			t.Error("error should be context.DeadlineExceeded")
+		if err != ErrNotMoreThanHalfNodes {
+			t.Error("error should be ErrNotMoreThanHalfNodes")
 		}
-
 		if errChan != nil {
 			t.Error("errChan should be nil")
 		}
-
-		if unlock != nil {
-			t.Error("unlock should nil")
+		err = unlock(ctx)
+		if err != nil {
+			t.Error("unlock error")
 		}
 	})
 
@@ -379,26 +349,26 @@ func TestTryLockWithAutoRenewal(t *testing.T) {
 			t.Error("lock error")
 		}
 
-		wg := sync.WaitGroup{}
-		wg.Add(1)
-		go func() {
-			for v := range errChan {
-				t.Error("errChan send a error:", v)
+		go func(errChan <-chan error) {
+			for range errChan {
+				t.Error("errChan send a error")
 			}
+		}(errChan)
 
-			wg.Done()
-		}()
+		defer unlock(ctx)
 
-		_, errChan, err = l.TryLockWithAutoRenewal(ctx)
-		if err != ErrLockIsAcquired {
-			t.Error("error should be ErrLockIsAcquired")
+		unlock, errChan, err = l.TryLockWithAutoRenewal(ctx)
+		if err != ErrNotMoreThanHalfNodes {
+			t.Error("error should be ErrNotMoreThanHalfNodes")
 		}
 
 		if errChan != nil {
 			t.Error("errChan should be nil")
 		}
 
-		unlock(ctx)
-		wg.Wait()
+		err = unlock(ctx)
+		if err != nil {
+			t.Error("unlock error")
+		}
 	})
 }
